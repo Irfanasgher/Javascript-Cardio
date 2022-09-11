35d6edc95d0da6cb4c82c995ac3924b3f6054621
Irfan Asgher <39435767+Irfanasgher@users.noreply.github.com>
2022-09-11 18:55:57 +0500

    first commit

---
+++ session1/index_start.js
@@ -0,0 +1,93 @@
+// CHALLENGE 1: REVERSE A STRING
+// Return a string in reverse
+// ex. reverseString('hello') === 'olleh'
+
+function reverseString(str) {
+	// return str.split("").reverse().join("");
+
+	//////////////////////////
+
+	let revString = "";
+	for (let i = str.length - 1; i >= 0; i--) {
+		revString = revString + str[i];
+	}
+	return revString;
+
+	/////////////////////////
+	// return 'Let\'s Start'
+}
+
+// CHALLENGE 2: VALIDATE A PALINDROME
+// Return true if palindrome and false if not
+// ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
+
+function isPalindrome(str) {}
+
+// CHALLENGE 3: REVERSE AN INTEGER
+// Return an integer in reverse
+// ex. reverseInt(521) === 125
+
+function reverseInt(int) {}
+
+// CHALLENGE 4: CAPITALIZE LETTERS
+// Return a string with the first letter of every word capitalized
+// ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
+function capitalizeLetters(str) {}
+
+// CHALLENGE 5: MAX CHARACTER
+// Return the character that is most common in a string
+// ex. maxCharacter('javascript') == 'a'
+function maxCharacter(str) {
+	const charMap = {};
+	let maxNum = 0;
+	let maxChar = "";
+
+	str.split("").forEach(function (char) {
+		if (charMap[char]) {
+			charMap[char]++;
+		} else {
+			charMap[char] = 1;
+		}
+	});
+
+	for (let char in charMap) {
+		// debugger;
+		if (charMap[char] > maxNum) {
+			maxNum = charMap[char];
+			maxChar = char;
+		}
+	}
+
+	return maxChar;
+
+	// const charMap = {};
+	// let maxNum = 0;
+	// let maxChar = "";
+	// const data = [6, 5, 7, 5, 8, 9, 5];
+	// data.forEach(function (char) {
+	// 	if (charMap[char]) {
+	// 		charMap[char]++;
+	// 	} else {
+	// 		charMap[char] = 1;
+	// 	}
+	// });
+
+	// for (let char in charMap) {
+	// 	// debugger;
+	// 	if (charMap[char] > maxNum) {
+	// 		maxNum = charMap[char];
+	// 		maxChar = char;
+	// 	}
+	// }
+
+	// return maxChar;
+}
+
+// CHALLENGE 6: FIZZBUZZ
+// Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
+function fizzBuzz() {}
+
+// Call Function
+const output = maxCharacter("javascript");
+
+console.log(output);
